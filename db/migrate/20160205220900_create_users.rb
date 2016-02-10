03eaf6ddf2ca95f899bf5b6f809b76f6e6548072
Alex <alexandrburyy@gmail.com>
2016-02-10 23:52:26 +0300

    user

--- db/migrate/20160205220900_create_users.rb
+++ db/migrate/20160205220900_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-
+      add_index :users, :email, unique: true
+      add_column :users, :password_digest, :string
       t.timestamps null: false
     end
   end
